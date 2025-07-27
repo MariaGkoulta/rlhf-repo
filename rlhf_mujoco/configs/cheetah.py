48dd17d25641e079eb400768df673db1da0b44f1
mgkoulta <marygkoulta@gmail.com>
2025-07-27 10:23:37 +0200

    Modify cheetah config file

--- rlhf_mujoco/configs/cheetah.py
+++ rlhf_mujoco/configs/cheetah.py
@@ -6,9 +6,11 @@ SEGMENT_LEN = 40 # Length of segments to extract from clips
 INITIAL_SEGMENT_LEN = 40 # Length of segments to extract from clips
 FINAL_SEGMENT_LEN = 40 # Length of segments to extract from clips
 
-TOTAL_TARGET_PAIRS = 1000
+TOTAL_TARGET_PAIRS = 750
 TOTAL_PPO_TIMESTEPS = 5e6
 
-FEEDBACK_TYPE = "evaluative"
+FEEDBACK_TYPE = "evaluative" # Type of feedback to use
 EVALUATIVE_RATING_RANGE=(-30, 120)
 EVALUATIVE_RATING_SCALE=None
+USE_PROBABILISTIC_MODEL = True  
+INITIAL_COLLECTION_FRACTION = 0.15
