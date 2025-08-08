2fe8c95d93121047ed549da1b9850a70aa291e4f
Maria Gkoulta <mgkoulta@eu-login-12.euler.ethz.ch>
2025-08-08 14:04:32 +0200

    Change environment hyperparameters

--- rlhf_mujoco/configs/cheetah.py
+++ rlhf_mujoco/configs/cheetah.py
@@ -6,11 +6,11 @@ SEGMENT_LEN = 40 # Length of segments to extract from clips
 INITIAL_SEGMENT_LEN = 40 # Length of segments to extract from clips
 FINAL_SEGMENT_LEN = 40 # Length of segments to extract from clips
 
-TOTAL_TARGET_PAIRS = 750
+TOTAL_TARGET_PAIRS = 500
 TOTAL_PPO_TIMESTEPS = 5e6
 
-FEEDBACK_TYPE = "evaluative" # Type of feedback to use
+FEEDBACK_TYPE = "preference" # Type of feedback to use
 EVALUATIVE_RATING_RANGE=(-30, 120)
 EVALUATIVE_RATING_SCALE=None
-USE_PROBABILISTIC_MODEL = True  
-INITIAL_COLLECTION_FRACTION = 0.15
+USE_PROBABILISTIC_MODEL = False  
+INITIAL_COLLECTION_FRACTION = 0.1
